61b228ec350dcc09d8f32bfcef80353427f25363
wanderingcar <69665475+wanderingcar@users.noreply.github.com>
2021-06-12 21:14:14 +0900

    Update main.py
    Added basic skeleton.

--- main.py
+++ main.py
@@ -1,1 +1,32 @@
+import simpy
+import numpy as np
 
+num_people = 0
+
+
+def IAT():
+    return np.random.exponential(scale=20)
+
+
+def service_time():  # 1인당 대피 소요시간
+    return np.random.exponential(scale=20)
+
+
+class Person:  # 사람
+    def __init__(self, env):
+        self.env = env
+        self.time = service_time()
+        self.index = num_people
+
+    def escape(self, env):
+        yield self.env.timeout(self.time)
+        print("")
+
+
+class Exit:  # 출구
+    def __init__(self, env):
+        self.env = env
+
+
+env = simpy.Environment()
+wayout = simpy.Resource(env, capacity=4)  # 출구
